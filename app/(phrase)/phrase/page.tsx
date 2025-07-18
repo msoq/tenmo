b58c49f34e22d45b6274f734d75b00dd4b07596d
Mikalai Sokarau <mikalaisokarau@gmail.com>
2025-07-18 15:49:25 +0200

    feat: add sidebar toggle to phrase page

--- app/(phrase)/phrase/page.tsx
+++ app/(phrase)/phrase/page.tsx
@@ -1,3 +1,16 @@
-export default async function Page() {
-  return <div>hallo phrase</div>;
+'use client';
+
+import { SidebarToggle } from '@/components/sidebar-toggle';
+
+export default function Page() {
+  return (
+    <div className="flex flex-col min-w-0 h-dvh bg-background">
+      <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
+        <SidebarToggle />
+      </header>
+      <main className="flex-1 p-4">
+        <div>hallo phrase</div>
+      </main>
+    </div>
+  );
 }
